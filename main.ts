0a88faefff2fddd32e99206b5094f493b2134cda
Noah-Yang666 <noah.yang@stu.ocsb.ca>
2026-02-17 13:13:28 -0500

    changed formatting

--- main.ts
+++ main.ts
@@ -4,10 +4,12 @@
  * Created on: Feb 2026
  * This program does basic math
 */
+
 //perimeter
 basic.showString('A computer has dimensions of 5 and 3cm')
 basic.showString("Perimeter: 2(5+3) = " + (2 * (5 + 3)) + " cm")
 basic.pause(500)
+
 //area
 basic.showString('A computer has dimensions of 5 and 3cm')
 basic.showString('Area: 5x3 =' + (5*3) + 'cm^2')
