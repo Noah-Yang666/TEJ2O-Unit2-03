ef7788d679d84bbf921714007244cf141ce90e02
Noah-Yang666 <noah.yang@stu.ocsb.ca>
2026-02-17 13:09:49 -0500

    program does basic math

--- main.ts
+++ main.ts
@@ -1,8 +1,13 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Noah Yang
+ * Created on: Feb 2026
+ * This program does basic math
 */
-
-basic.showString('Hello, World!')
+//perimeter
+basic.showString('A computer has dimensions of 5 and 3cm')
+basic.showString("Perimeter: 2(5+3) = " + (2 * (5 + 3)) + " cm")
+basic.pause(500)
+//area
+basic.showString('A computer has dimensions of 5 and 3cm')
+basic.showString('Area: 5x3 =' + (5*3) + 'cm^2')
